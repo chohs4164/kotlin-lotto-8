2b1ab208473747792e3a73d79916fe2561069cb1
chohs4164 <josh100as@gmail.com>
2025-11-01 17:58:32 +0900

    feat(Lotto): 로또 로직이 굴러갈 수 있도록 하는 컨트롤러 구현
    - 구입 금액 입력받아 로또 구매 갯수를 파악하고 랜덤 번호 출력해주고 당첨 번호 + 보너스 번호와 비교하며 맞은 갯수와 수익률 출력
    
    Refs: #4

---
+++ src/main/kotlin/controller/LottoController.kt
@@ -0,0 +1,30 @@
+package controller
+
+
+import domain.LottoGenerator
+import util.Validator
+import view.InputView
+import view.OutputView
+
+//로또 몇 줄을 만들지 결정만 함.
+class LottoController() {
+    fun start() {
+        // 구입 금액 알아내고
+        val inputPrice = InputView.readLottoPrice()
+        // 갯수로 변환하여
+        val lottoCount = inputPrice / 1000
+        // 갯수 만큼의 로또 줄을 만들고
+        val lottos = LottoGenerator.createLottos(lottoCount)
+        // 로또 구매 갯수와 랜덤 숫자들 출력
+        OutputView.printLottoNumber(lottoCount, lottos)
+        // 진짜 당첨 번호 입력하고
+        val inputWinNumber = InputView.readWinNumber()
+        println()
+        // 보너스 번호도 입력하고
+        val inputBonusNumber = InputView.readBonusNumber(inputWinNumber)
+        println()
+        // 몇 개 맞았는지 확인
+        val validateCount = Validator.validateNumber(lottos, inputWinNumber, inputBonusNumber)
+        OutputView.printWinStatistics(lottoCount, validateCount)
+    }
+}
