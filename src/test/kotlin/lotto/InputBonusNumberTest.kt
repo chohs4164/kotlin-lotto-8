1613bf5da0743634d20ab9571fb1779fd6928d45
chohs4164 <josh100as@gmail.com>
2025-11-01 17:30:41 +0900

    test: 예외처리 테스트 케이스 작성
     - 보너스 번호로 공백이 들어오면 예외처리
     - 보너스 번호로 들어온 문자를 숫자로 변환하고자 했을 때 숫자가 아니면 예외처리
     - 보너스 번호로 로또 범위를 벗어난 보너스 번호 입력 시 예외처리
     - 보너스 번호와 당첨 번호가 중복될 시 예외처리
    
    Refs: #3

---
+++ src/test/kotlin/lotto/InputBonusNumberTest.kt
@@ -0,0 +1,14 @@
+package lotto
+
+import org.junit.jupiter.api.Test
+
+class InputBonusNumberTest {
+    @Test
+    fun `보너스 번호로 들어온 문자를 숫자로 변환하고자할 때 숫자가 아닌경우`(){
+
+    }
+    @Test
+    fun `보너스 번호와 당첨 번호가 중복될 경우`(){
+
+    }
+}
