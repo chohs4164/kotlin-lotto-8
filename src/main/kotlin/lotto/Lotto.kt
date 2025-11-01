d98d6349cf76909b769071c79116cc595438bd7b
chohs4164 <josh100as@gmail.com>
2025-11-01 17:43:16 +0900

    feat(Lotto): 로또 객체 클래스 구현
    - 숫자 6개(로또 1줄)를 리스트 형식으로 받아서 저장
    - getter를 통해 각 숫자 요소에 접근
    
    Refs: #1

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,15 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
+// 생성한 로또 번호 1줄을 담을 lotto 객체
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
     }
 
     // TODO: 추가 기능 구현
+    fun getNumbers():List<Int>{
+        return numbers
+    }
 }
