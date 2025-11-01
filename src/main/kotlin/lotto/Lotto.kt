2a898a36e331019e4e6f4910c1f62ef642f049ac
chohs4164 <josh100as@gmail.com>
2025-11-02 00:49:51 +0900

    feat(Lotto): Lotto.kt 예외처리 로직 추가
    - 로또 번호 중복과 범위를 벗어난 로또 번호 확인에 대한 로직 추가
    
    Refs: #1

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -6,10 +6,12 @@ import camp.nextstep.edu.missionutils.Randoms
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.distinct().size == 6) { "[ERROR] 로또 번호에 중복된 숫자가 있습니다." }
+        require(numbers.all { it in 1..45 }) { "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다." }
     }
 
     // TODO: 추가 기능 구현
-    fun getNumbers():List<Int>{
+    fun getNumbers(): List<Int> {
         return numbers
     }
 }
