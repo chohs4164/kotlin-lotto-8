c5b63e229365097f9d7263a12123c502dd5c4439
chohs4164 <josh100as@gmail.com>
2025-11-02 00:42:29 +0900

    fix(Lotto): 로또 번호 비교 로직 수정
    - for문 안으로 result를 넣어서 한 줄 한 줄 검사한 결과를 누적하여 계산하도록 수정
    
    Refs: #6

--- src/main/kotlin/util/Validator.kt
+++ src/main/kotlin/util/Validator.kt
@@ -3,9 +3,10 @@ package util
 import lotto.Lotto
 
 object Validator {
+    // lottos: 구매한 로또 번호 리스트들, inputWinNumber: 당첨 로또 번호, inputBonusNumber: 당첨 보너스 번호
     fun validateNumber(lottos: List<Lotto>, inputWinNumber: Lotto, inputBonusNumber: Int): MutableList<Pair<Int, Int>> {
         val result = mutableListOf<Pair<Int, Int>>()
-        var winCount: Int = 0
+        var winCount = 0
         var bonusCount = 0
         for (lotto in lottos) {
             // 이미 오름차순으로 랜덤 번호를 정렬해놓았으니 이렇게 일치하는지 계산
@@ -13,8 +14,8 @@ object Validator {
             // 보너스 번호도 계산
             if (inputBonusNumber in lotto.getNumbers())
                 bonusCount++
+            result.add(Pair(winCount, bonusCount))
         }
-        result.add(Pair(winCount, bonusCount))
         return result
     }
 }
