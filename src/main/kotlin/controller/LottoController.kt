74c216136297bba4f782ea3b071c36ab492df7ec
chohs4164 <josh100as@gmail.com>
2025-11-02 00:48:06 +0900

    fix(Lotto): 컨트롤러 예외처리 방식 수정
    - try catch문으로 감싸고 println을 추가하여 에러 메시지가 출력되도록 수정
    
    Refs: #4

--- src/main/kotlin/controller/LottoController.kt
+++ src/main/kotlin/controller/LottoController.kt
@@ -10,21 +10,23 @@ import view.OutputView
 class LottoController() {
     fun start() {
         // 구입 금액 알아내고
-        val inputPrice = InputView.readLottoPrice()
-        // 갯수로 변환하여
-        val lottoCount = inputPrice / 1000
-        // 갯수 만큼의 로또 줄을 만들고
-        val lottos = LottoGenerator.createLottos(lottoCount)
-        // 로또 구매 갯수와 랜덤 숫자들 출력
-        OutputView.printLottoNumber(lottoCount, lottos)
-        // 진짜 당첨 번호 입력하고
-        val inputWinNumber = InputView.readWinNumber()
-        println()
-        // 보너스 번호도 입력하고
-        val inputBonusNumber = InputView.readBonusNumber(inputWinNumber)
-        println()
-        // 몇 개 맞았는지 확인
-        val validateCount = Validator.validateNumber(lottos, inputWinNumber, inputBonusNumber)
-        OutputView.printWinStatistics(lottoCount, validateCount)
+        try {
+            val inputPrice = InputView.readLottoPrice()
+            // 갯수로 변환하여
+            val lottoCount = inputPrice / 1000
+            // 갯수 만큼의 로또 줄을 만들고
+            val lottos = LottoGenerator.createLottos(lottoCount)
+            // 로또 구매 갯수와 랜덤 숫자들 출력
+            OutputView.printLottoNumber(lottoCount, lottos)
+            // 진짜 당첨 번호 입력하고
+            val inputWinNumber = InputView.readWinNumber()
+            // 보너스 번호도 입력하고
+            val inputBonusNumber = InputView.readBonusNumber(inputWinNumber)
+            // 몇 개 맞았는지 확인
+            val validateCount = Validator.validateNumber(lottos, inputWinNumber, inputBonusNumber)
+            OutputView.printWinStatistics(lottoCount, validateCount)
+        }catch (e: IllegalArgumentException){
+            println(e.message)
+        }
     }
 }
